3a68fd3b5f4a1dd77bfec50d385e9a79ef7de914
dakookie <damaris.suquillo@epn.edu.ec>
2025-08-05 12:06:17 -0500

    Corrections in xml files

--- app/src/main/java/com/jimenez/edison/shopplan/UserActivity.kt
+++ app/src/main/java/com/jimenez/edison/shopplan/UserActivity.kt
@@ -1,12 +1,16 @@
 package com.jimenez.edison.shopplan
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class UserActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +19,13 @@ class UserActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val signOutButton = findViewById<Button>(R.id.user_sign_out_button)
+        signOutButton.setOnClickListener {
+            val intent = Intent(this, Login::class.java)
+            startActivity(intent)
+            finish()
         }
     }
 }
